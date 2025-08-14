fcd8532fa650d8a0e54d7954a9261180a8507de8
Matin Hosz <matin.hosseinzadeh06@gmail.com>
2025-08-14 16:22:55 +0330

    Revert "Create database tables"
    This reverts commit 00d51947f442b33c8e0a083790b8b9f995e15333.

--- EmailServiceSim/src/main/resources/db/email_recipients.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS email_recipients(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    email_id INT NOT NULL,
-    recipient_id INT NOT NULL,
-
-    FOREIGN KEY (email_id) REFERENCES emails(id),
-    FOREIGN KEY (recipient_id) REFERENCES people(id)
-);
