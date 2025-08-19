46caa92d47d7942504465f67d1e5686432083f9b
Matin Hosz <matin.hosseinzadeh06@gmail.com>
2025-08-19 15:46:11 +0330

    Rewrite service classes based on new design pattern

--- EmailServiceSim/src/main/java/aut/ap/service/EmailService.java
+++ EmailServiceSim/src/main/java/aut/ap/service/EmailService.java
@@ -1,25 +1,45 @@
 package aut.ap.service;
 
-import aut.ap.framework.ServiceBase;
+import java.util.List;
+import java.util.regex.Pattern;
+
 import aut.ap.model.Email;
 import aut.ap.model.Person;
-import java.util.List;
+import aut.ap.repository.EmailRepository;
 
-public class EmailService extends ServiceBase<Email> {
+public class EmailService {
+    private static final Pattern PREFIX_PATTERN = Pattern.compile("^((\\[(Re|Fw)\\]\\s)+)");
+    public EmailRepository repository;
 
     public EmailService() {
-        super(Email.class);
+        repository = new EmailRepository(Email.class);
     }
 
-    public Email persist() {
+    public void reply(Email orginalEmail, Person replyingSender, String body) {
+        List<Person> recipients = repository.getRecipients(orginalEmail.getId());
+        recipients.remove(replyingSender);
+        recipients.add(orginalEmail.getSender());
 
+        String subject = "[Re] " + getCleanSubject(orginalEmail);
+        Email repliedEmail = new Email(replyingSender, subject, body, orginalEmail, "reply");
+
+        repository.persistEmail(repliedEmail);
+        repository.send(recipients, repliedEmail.getId());
     }
 
-    public Email fetchByCode(String code) {
-        return getSessionFactory().fromTransaction(session -> {
+    public void forward(List<Person> recipients, Email orginalEmail, Person sender) {
 
-        });
+        String subject = "[Fw] " + EmailService.getCleanSubject(orginalEmail);
+        Email forwardedEmail = new Email(sender, subject, orginalEmail.getBody(), orginalEmail, "forward");
+
+        repository.persistEmail(forwardedEmail);
+        repository.send(recipients, forwardedEmail.getId());
     }
 
-    // TODO: Complete this class after refactoring emails read status
+    public static String getCleanSubject(Email email) {
+        String subject = email.getSubject();
+
+        // Find the pattern at the start of the subject and replace it with ""
+        return PREFIX_PATTERN.matcher(subject).replaceFirst("");
+    }
 }
--- EmailServiceSim/src/main/java/aut/ap/service/PersonService.java
+++ EmailServiceSim/src/main/java/aut/ap/service/PersonService.java
@@ -1,29 +1,37 @@
 package aut.ap.service;
 
-import aut.ap.framework.ServiceBase;
+import aut.ap.exception.InvalidEmailException;
 import aut.ap.model.Person;
+import aut.ap.repository.PersonRepository;
 
-public class PersonService extends ServiceBase<Person> {
+public class PersonService {
+    public PersonRepository repository;
 
     public PersonService() {
-        super(Person.class);
+        repository = new PersonRepository(Person.class);
     }
 
-    public Person persist(String name, String email, String password) {
+    public void register(String name, String email, String password) {
+        if (repository.fetchByEmail(email) != null)
+            throw new InvalidEmailException("An account already exists with email: " + email);
+        if (password.length() < 8)
+            throw new IllegalArgumentException("Password must be at least 8 characters long.");
+        
+        if (!email.contains("@"))
+            email = email + "@milou.com";
+        
         Person person = new Person(name, email, password);
-        persist(person);
-
-        return person;
+        repository.persistPerson(person);
     }
 
-    public Person fetchByEmail(String email) {
-        return getSessionFactory().fromTransaction(session -> {
-            return session.createSelectionQuery("from Person where email like :email1 or email like :email2", Person.class)
-                    .setParameter("emailParametr1", email)
-                    .setParameter("emailParametr2", email + "@milou.com")
-                    .getSingleResult();
-                    
-                    // TODO: getSingleResult will throw exception -> handle it and others
-        });
+    public Person login(String email, String password) {
+        Person person = repository.fetchByEmail(email);
+        if (person == null) {
+            throw new InvalidEmailException("No account found with email: " + email);
+        }
+        if (!person.getPassword().equals(password)) {
+            throw new IllegalArgumentException("Invalid password.");
+        }
+        return person;
     }
 }
